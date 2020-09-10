ad6a8a865b43bb5a39b520575b512272e4f43069
Hongli Zhang <hzhang@narrativescience.com>
2020-09-10 17:26:11 -0500

    testeteset

--- factorial/tests/unit/factorial_test.py
+++ factorial/tests/unit/factorial_test.py
@@ -14,6 +14,14 @@ class TestFactorial(unittest.TestCase):
         res = fact(5)
         self.assertEqual(res, 120)
 
+    def test_fact_0(self):
+        """
+        The actual test.
+        Any method which starts with ``test_`` will considered as a test case.
+        """
+        res = fact(0)
+        self.assertEqual(res, 1)
+
     def test_div(self):
         """
         The actual test.
